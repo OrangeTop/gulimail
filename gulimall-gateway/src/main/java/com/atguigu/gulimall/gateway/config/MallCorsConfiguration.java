15e22561455c159916c76c44d8662d246bfb8cb7
orangeTop <orangetop@foxmail.com>
2022-02-07 13:18:42 +0800

    P65

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/MallCorsConfiguration.java
@@ -0,0 +1,33 @@
+package com.atguigu.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+
+/**
+ * @Description:
+ * @Author: ChengRG
+ * @Date: 2022/1/22 22:06
+ */
+@Configuration
+public class MallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // 配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,8 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -14,6 +17,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -24,6 +32,34 @@ public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService service;
+
+//    @Autowired
+//    OSSClient ossClient;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        
+//        // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
+//        String objectName = "C:\\Users\\75173\\Pictures\\Camera Roll\\bluesky.png";
+//
+//        // 创建OSSClient实例。
+//        String endpoint = "oss-cn-beijing.aliyuncs.com";
+//        String accessId = "LTAI5t8jYT3Frk9npajTcDoQ";
+//
+//        String secretId = "y5ghrIX7FMhXWRktExhl8OKjAAVogP";
+//
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessId, secretId);
+//
+//        String content = "Hello OSS";
+//        String bucket = "gulimall-ora";
+////        ossClient.putObject(bucket, objectName, new ByteArrayInputStream(content.getBytes()));
+////
+////        // 关闭OSSClient。
+////        ossClient.shutdown();
+//        InputStream inputStream = new FileInputStream(objectName);
+//        ossClient.putObject(bucket,"aa.jpg",inputStream);
+//        ossClient.shutdown();
+    }
 
     @Test
     public void contextLoads(){
